8f9deea4e907288cea1ee9f124d2555821e5d5fd
bolt433 <ifow_barca@live.no>
2021-11-05 17:56:40 +0100

    Action test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,13 @@
 public class main extends Program{
     public static void main(String[] args){
         Leap_Year(1700);
+
+        String j;
+        String Ni;
+        System.out.println(1800 % 4);
+        System.out.println(1800 % 100);
+
+
     }
+
 }
